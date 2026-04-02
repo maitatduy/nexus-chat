bff2c19e645dd1978512150c2290c8208f2f2c4a
maitatduy <maitatduy.work@gmail.com>
2026-04-02 15:26:00 +0700

    refactor: move validations folder to src directory

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { signIn, signUp } from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/validate.middleware.js";
-import { signInSchema, signUpSchema } from "../../validations/auth.validation.js";
+import { signInSchema, signUpSchema } from "../validations/auth.validation.js";
 
 const router = express.Router();
 
