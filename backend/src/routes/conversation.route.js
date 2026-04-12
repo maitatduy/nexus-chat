47902d3b81198d0e56ba4ffa11e4da53057880cf
maitatduy <maitatduy.work@gmail.com>
2026-04-12 12:34:59 +0700

    fix: correct middleware function call in message routes

--- backend/src/routes/conversation.route.js
+++ backend/src/routes/conversation.route.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { createConversation, getConversations, getMessages } from "../controllers/conversation.controller.js";
-import { checkFriendship, checkGroupMembership } from "../middlewares/friend.middleware.js";
+import { checkFriendship } from "../middlewares/friend.middleware.js";
 
 const router = express.Router();
 
 router.post("/", checkFriendship, createConversation);
-router.get("/", checkGroupMembership, getConversations);
+router.get("/", getConversations);
 router.get("/:conversationId/messages", getMessages);
 
 export default router;
