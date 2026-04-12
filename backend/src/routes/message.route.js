50530efeac16506f5fdc0dc988927045175ec401
maitatduy <maitatduy.work@gmail.com>
2026-04-12 07:00:17 +0700

    feat: add middleware to verify friendship between users

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { sendDirectMessage, sendGroupMessage } from "../controllers/message.controller.js";
+import { checkFriendship } from "../middlewares/friend.middleware.js";
 
 const router = express.Router();
 
-router.post("/direct", sendDirectMessage);
+router.post("/direct", checkFriendship, sendDirectMessage);
 router.post("/group", sendGroupMessage);
 
 export default router;
